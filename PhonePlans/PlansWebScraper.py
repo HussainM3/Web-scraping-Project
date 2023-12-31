1f124de6168b463573b8b4aa78596df2e0588292
Hussain Muhammed <muhammeh@mcmaster.ca>
2023-12-31 00:54:08 -0500

    - imported By to fix finding element function usage for newer selenium version - getting error in gathering deals

--- PhonePlans/PlansWebScraper.py
+++ PhonePlans/PlansWebScraper.py
@@ -3,6 +3,7 @@
 # For this reason use selenium
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Get url of page you want to scrape from
 rogersUrl = "https://www.rogers.com/plans#mobile-plans" #NOTE: use helium for getting full deal details
@@ -11,7 +12,7 @@ rogersUrl = "https://www.rogers.com/plans#mobile-plans" #NOTE: use helium for ge
 driver = webdriver.Chrome()
 
 # run website
-# driver.get(rogersUrl)
+driver.get(rogersUrl)
 
 
 
@@ -22,7 +23,10 @@ driver = webdriver.Chrome()
 # Perks: //*[@id="converge"]/dsa-layout/div/div/rci-ui-block-wrapper/div/div/rci-ui-block-tile-plans/div/div/div[1]/dsa-vertical-tile/ds-tile/div/div/div[2]/ul/li[2]/p[2]
 
 # Gather all deals (all have tile with same class name)
-deals = driver.find_elements('col-12 col-sm-6 col-md-4 mb-24 mt-sm-0 mt-md-0 ng-star-inserted')
+#Note: newer version of selenium so must specify getting elements By XPATH (Must also import 'By')
+deals = driver.find_elements(By.CLASS_NAME, 'col-12 col-sm-6 col-md-4 mb-24 mt-sm-0 mt-md-0 ng-star-inserted')
 
-for deal in deals:
-    print(deal)
+print(deals)
+
+# for deal in deals:
+#     print(deal)
