3d3f09426df65059c5ace8b9ba6e181d56b4570d
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-01-03 23:22:21 -0500

    fixed getting plans info

--- PhonePlans/getPlansInfo.js
+++ PhonePlans/getPlansInfo.js
@@ -10,38 +10,39 @@ function getFileData(){
     var data = fs.readFileSync('PhonePlans/plans.txt', 'utf8') 
 
     // array to be used for each plan
-    const plansData = [];
+    const plansArray = [];
 
     // split data into carrier sections (remove empty sections)
     var plansSections = data.split('**************************************************');
     plansSections = plansSections.slice(1,plansSections.length - 1);
     
     // loop through each section
-    for (var section of plansSections){
-        // dictionary for each plan
-        plan = {}
-        
+    for (var section of plansSections){        
         // getting carrier
-        const carrier = section.substring(0, section.indexOf(':'));
+        const carrier = section.substring(0, section.indexOf(':')).trim();
 
         // trimming section to just plans
         section = section.substring(section.indexOf('Name:'));
         
         // splitting each plan into list
         subSections = section.split("\n\n")
+        subSections = subSections.slice(0, subSections.length - 1);
 
         // iterating through each plan and collecting info
         for (var eachPlanInfo of subSections){
+            // dictionary for each plan
+            plan = {};
             plan.carrier = carrier;
             plan.name = eachPlanInfo.substring(eachPlanInfo.indexOf('Name:') + 6, eachPlanInfo.indexOf('\nPrice:'));
             plan.price = eachPlanInfo.substring(eachPlanInfo.indexOf('Price:') + 7, eachPlanInfo.indexOf('\nDesc'));
-            plan.desc = eachPlanInfo.substring(eachPlanInfo.indexOf('Description:') + 13, eachPlanInfo.indexOf('\nFeatures:'));
-            plan.features = eachPlanInfo.substring(eachPlanInfo.indexOf('Features:') + 10, eachPlanInfo.indexOf('\nPerks:'));
-            plan.perks = eachPlanInfo.substring(eachPlanInfo.indexOf('Perks:') + 7, eachPlanInfo.indexOf('\n\n'));
+            plan.desc = eachPlanInfo.substring(eachPlanInfo.indexOf('Description:') + 13, eachPlanInfo.indexOf('\nFeatures:')).replace(/\n/g, '; ');
+            plan.features = eachPlanInfo.substring(eachPlanInfo.indexOf('Features:') + 10, eachPlanInfo.indexOf('\nPerks:')).replace(/\n/g, '; ');
+            plan.perks = eachPlanInfo.substring(eachPlanInfo.indexOf('Perks:') + 7).replace(/\n/g, '; ');
+            plansArray.push(plan);
         }
     }
-    
+    return plansArray;
 }
-
-getFileData();
+var fileData = getFileData();
+console.log(fileData);
 
