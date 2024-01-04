475d610b33e63e616715dbe52fa473fbe482d608
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-01-03 21:29:44 -0500

    first draft of getting file data func

--- PhonePlans/getPlansInfo.js
+++ PhonePlans/getPlansInfo.js
@@ -1,3 +1,19 @@
 // File to be used for reading phone plans for displaying on website through html file
-var fs = require('fs');
+
+const { get } = require('http');
+
+// function for reading file and getting phone plan content
+function getFileData(){
+    var fs = require('fs');
+
+    // reads file and saves to variable 
+        // first param is file to read
+        // second param is call back which contains err (if exists) and data read
+    const allData = fs.readFile('/plans.txt', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
+}
+
+getFileData();
 
